a8644b37f36d2db1997458520294126970089913
tikavalery <43121065+tikavalery@users.noreply.github.com>
2018-12-22 17:21:03 -0800

    friendfinder assigment

---
+++ app/data/friends.js
@@ -0,0 +1,166 @@
+// Data structure that will store the friends data
+var friendsArray = [
+	{
+		name: 'Andrew Carnegie',
+		photo: 'https://cdn.britannica.com/s:500x350/95/166595-049-EAE7F534.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Henry Ford',
+		photo: 'https://www.thehenryford.org/images/default-source/explore/henry-ford.jpg?Status=Master&sfvrsn=6ed62e01_2',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Elon Musk',
+		photo: 'https://thenypost.files.wordpress.com/2018/08/180817-elon-musk-interview-feature.jpg?quality=90&strip=all&w=618&h=410&crop=1',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Zackariah',
+		photo: 'https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Cloe',
+		photo: 'http://4.bp.blogspot.com/-HTvSYzA-pO4/UgQb4Zh_u0I/AAAAAAAAEuI/XwhtogT_1tA/s1600/3+cute2.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Nelson Mandela',
+		photo: 'https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU3OTIzNTc3OTk0NzQ5NTg2/remembering-nelson-mandelas-featured-photo.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Beyonce Knowles',
+		photo: 'http://www.billboard.com/files/styles/article_main_image/public/media/beyonce-feb-2014-billboard-650.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Elmo',
+		photo: 'http://1rjm5qg4v8732kpmn44ya6bj-wpengine.netdna-ssl.com/wp-content/uploads/2012/11/elmo-6601.png',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Kelly Rowland',
+		photo: 'https://cdnph.upi.com/svc/sv/upi/2061532613885/2018/1/b04263c0470a54e6e1dac69e488cc100/Kelly-Rowland-supports-Demi-Lovato-after-reported-overdose.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Thomas Edison',
+		photo: 'https://cdn-images-1.medium.com/max/1186/1*s2GyMoLeSV3Epc2Gk3qBXA.png',
+
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
+
+module.exports = friendsArray;
